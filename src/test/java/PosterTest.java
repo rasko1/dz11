1113431d581b3c746326ffc127b67b164005a1f7
rasko1 <oophn.88@yandex.ru>
2023-06-21 21:26:16 +0300

    mvn

--- src/test/java/PosterTest.java
+++ src/test/java/PosterTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class PosterTest {
     @Test
     public void Poster() {
@@ -48,7 +47,6 @@ public class PosterTest {
 
         String[] expected = {"Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
         String[] actual = manager.findLast();
-
         Assertions.assertArrayEquals(expected, actual);
     }
     @Test
